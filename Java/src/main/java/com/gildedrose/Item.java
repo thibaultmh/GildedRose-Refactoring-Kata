bcdcba2b2cecf2b77abc3bcb177c1592a54bd74a
Thibault <thibault.mahieu@mips.be>
2021-07-04 14:56:26 +0200

    Update quality refactor proposal
    Added update quality refactor proposal.

--- Java/src/main/java/com/gildedrose/Item.java
+++ Java/src/main/java/com/gildedrose/Item.java
@@ -23,8 +23,48 @@ public class Item {
         this.quality = quality;
     }
 
-   @Override
-   public String toString() {
+    @Override
+    public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
     }
+
+    /**
+     * Updates Item SellIn and Quality.
+     */
+    public void update() {
+        // TMH: we couldn't alter item but I figured adding extra functionality would be OK since it can't break old code. This function is the cleanest solution I think ...
+        // TMH: I would make this an interface or abstract method but for backwards compatibility assume this is a normal item
+        // TODO
+    }
+
+    /**
+     * Gets the item name.
+     *
+     * @return the name
+     */
+    public String getName() {
+        // TMH: we couldn't alter item but I figured adding extra functionality would be OK since it can't break old code.
+        return name;
+    }
+
+    /**
+     * Gets the the number of days we have to sell the item.
+     *
+     * @return the sellIn
+     */
+    public int getSellIn() {
+        // TMH: we couldn't alter item but I figured adding extra functionality would be OK since it can't break old code.
+        return sellIn;
+    }
+
+    /**
+     * Gets the item quality.
+     *
+     * @return the quality
+     */
+    public int getQuality() {
+        // TMH: we couldn't alter item but I figured adding extra functionality would be OK since it can't break old code.
+        return quality;
+    }
+
 }
